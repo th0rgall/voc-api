1ed33d0dad53050bf6651e881912c2e67a88a437
Thor Galle <thor.galle@student.kuleuven.be>
2018-06-05 19:42:18 +0200

    File reorganisation + show definitions in dictionary pages

---
+++ google-translate.js
@@ -0,0 +1,319 @@
+/**
+ * Last update: 2016/06/26
+ * https://translate.google.com/translate/releases/twsfe_w_20160620_RC00/r/js/desktop_module_main.js
+ *
+ * Everything between 'BEGIN' and 'END' was copied from the url above.
+ * 
+ * SOURCE: https://github.com/matheuss/google-translate-token/blob/master/index.js
+ */
+
+// var got = require('got');
+// var Configstore = require('configstore');
+
+/* eslint-disable */
+// BEGIN
+
+function sM(a) {
+    var b;
+    if (null !== yr)
+        b = yr;
+    else {
+        b = wr(String.fromCharCode(84));
+        var c = wr(String.fromCharCode(75));
+        b = [b(), b()];
+        b[1] = c();
+        b = (yr = window[b.join(c())] || "") || ""
+    }
+    var d = wr(String.fromCharCode(116))
+        , c = wr(String.fromCharCode(107))
+        , d = [d(), d()];
+    d[1] = c();
+    c = "&" + d.join("") + "=";
+    d = b.split(".");
+    b = Number(d[0]) || 0;
+    for (var e = [], f = 0, g = 0; g < a.length; g++) {
+        var l = a.charCodeAt(g);
+        128 > l ? e[f++] = l : (2048 > l ? e[f++] = l >> 6 | 192 : (55296 == (l & 64512) && g + 1 < a.length && 56320 == (a.charCodeAt(g + 1) & 64512) ? (l = 65536 + ((l & 1023) << 10) + (a.charCodeAt(++g) & 1023),
+            e[f++] = l >> 18 | 240,
+            e[f++] = l >> 12 & 63 | 128) : e[f++] = l >> 12 | 224,
+            e[f++] = l >> 6 & 63 | 128),
+            e[f++] = l & 63 | 128)
+    }
+    a = b;
+    for (f = 0; f < e.length; f++)
+        a += e[f],
+            a = xr(a, "+-a^+6");
+    a = xr(a, "+-3^+b+-f");
+    a ^= Number(d[1]) || 0;
+    0 > a && (a = (a & 2147483647) + 2147483648);
+    a %= 1E6;
+    return c + (a.toString() + "." + (a ^ b))
+}
+
+var yr = null;
+var wr = function(a) {
+    return function() {
+        return a
+    }
+}
+    , xr = function(a, b) {
+    for (var c = 0; c < b.length - 2; c += 3) {
+        var d = b.charAt(c + 2)
+            , d = "a" <= d ? d.charCodeAt(0) - 87 : Number(d)
+            , d = "+" == b.charAt(c + 1) ? a >>> d : a << d;
+        a = "+" == b.charAt(c) ? a + d & 4294967295 : a ^ d
+    }
+    return a
+};
+
+// END
+/* eslint-enable */
+
+// var config = new Configstore('google-translate-api');
+
+/* 
+var window = {
+    TKK: config.get('TKK') || '0'
+};
+*/
+localStorage.setItem('TKK', localStorage.getItem('TKK') || '0');
+
+function withModifiedReferrer(refererUrl, requestUrl, action) {
+    function refererListener(details) {
+        const i = details.requestHeaders.findIndex(e => e.name.toLowerCase() == "referer");
+        if (i != -1) {
+            details.requestHeaders[i].value = refererUrl;
+        } else {
+            details.requestHeaders.push({name: "referer", value: refererUrl});
+        }
+        // Firefox uses promises
+        // return Promise.resolve(details);
+        // Chrome doesn't. Todo: https://github.com/mozilla/webextension-polyfill
+    
+        // important: do create a new object, passing the modified argument does not work
+        return {requestHeaders: details.requestHeaders};
+    }
+
+    // modify headers with webRequest hook
+    chrome.webRequest.onBeforeSendHeaders.addListener(
+    refererListener, //  function
+    {urls: [requestUrl]}, // RequestFilter object
+    ["requestHeaders", "blocking"] //  extraInfoSpec
+    );
+
+    // TODO:    why not hook detach after action? async?
+    //          hook should be detached after the request was sent, automatically   
+    action(() => {
+    // detach hook
+    if (chrome.webRequest.onBeforeSendHeaders.hasListener(refererListener)) {
+        chrome.webRequest.onBeforeSendHeaders.removeListener(refererListener)
+    }
+    });
+}
+
+function httpGet(requestUrl, type, modifiedReferrer) {
+    return new Promise( (resolve, reject) => {
+        let action = (detachHook) => {
+            var req = new XMLHttpRequest();
+            //req.withCredentials = true;
+            req.open("GET", requestUrl, true);
+            req.responseType = type ? type : "document";
+            req.onload = function () {
+                if (detachHook) detachHook();
+                if (req.status == 200) {
+                    resolve(req.response);
+                }
+                else if (req.status != 200) {
+                    reject();
+                }
+             }
+            req.send();
+        }
+        if (modifiedReferrer) {
+            chrome.runtime.sendMessage({
+                type: 'translation',
+                referrer: modifiedReferrer,
+                url: requestUrl
+            }, (res) => {
+                resolve(res);
+            });
+        } else {
+            action();
+        }
+    });
+}
+
+function updateTKK() {
+    return new Promise(function (resolve, reject) {
+        var now = Math.floor(Date.now() / 3600000);
+
+        //if (Number(window.TKK.split('.')[0]) === now) {
+        if (Number(localStorage.getItem('TKK').split('.')[0]) === now) {
+            resolve();
+        } else {
+            //got('https://translate.google.com').then(function (res) {
+            httpGet('https://translate.google.com').then(function (res) {
+                // var code = res.body.match(/TKK=(.*?)\(\)\)'\);/g);
+                var code = res.body.innerHTML.match(/TKK=(.*?)\(\)\)'\);/g);
+
+                if (code) {
+                    eval(code[0]);
+                    /* eslint-disable no-undef */
+                    if (typeof TKK !== 'undefined') {
+                        // window.TKK = TKK;
+                        // config.set('TKK', TKK);
+                        localStorage.setItem('TKK', TKK);
+                    }
+                    /* eslint-enable no-undef */
+                }
+
+                /**
+                 * Note: If the regex or the eval fail, there is no need to worry. The server will accept
+                 * relatively old seeds.
+                 */
+
+                resolve();
+            }).catch(function (err) {
+                var e = new Error();
+                e.code = 'BAD_NETWORK';
+                e.message = err.message;
+                reject(e);
+            });
+        }
+    });
+}
+
+function getGoogleToken(text) {
+    return updateTKK().then(function () {
+        var tk = sM(text);
+        tk = tk.replace('&tk=', '');
+        return {name: 'tk', value: tk};
+    }).catch(function (err) {
+        throw err;
+    });
+}
+
+// module.exports.get = get;
+
+/**
+ * API based on: https://github.com/matheuss/google-translate-api/blob/master/index.js
+ */
+
+/**
+ * from: https://github.com/Gozala/querystring/blob/master/encode.js
+ */
+function stringifyPrimitive(v) {
+    switch (typeof v) {
+      case 'string':
+        return v;
+  
+      case 'boolean':
+        return v ? 'true' : 'false';
+  
+      case 'number':
+        return isFinite(v) ? v : '';
+  
+      default:
+        return '';
+    }
+  };
+
+function stringify(obj, sep, eq, name) {
+    sep = sep || '&';
+    eq = eq || '=';
+    if (obj === null) {
+      obj = undefined;
+    }
+  
+    if (typeof obj === 'object') {
+      return Object.keys(obj).map(function(k) {
+        var ks = encodeURIComponent(stringifyPrimitive(k)) + eq;
+        if (Array.isArray(obj[k])) {
+          return obj[k].map(function(v) {
+            return ks + encodeURIComponent(stringifyPrimitive(v));
+          }).join(sep);
+        } else {
+          return ks + encodeURIComponent(stringifyPrimitive(obj[k]));
+        }
+      }).filter(Boolean).join(sep);
+  
+    }
+  
+    if (!name) return '';
+    return encodeURIComponent(stringifyPrimitive(name)) + eq +
+           encodeURIComponent(stringifyPrimitive(obj));
+  };
+
+function translate(text, opts) {
+    opts = opts || {};
+
+    var e;
+    [opts.from, opts.to].forEach(function (lang) {
+        if (lang && !isSupported(lang)) {
+            e = new Error();
+            e.code = 400;
+            e.message = 'The language \'' + lang + '\' is not supported';
+        }
+    });
+    if (e) {
+        return new Promise(function (resolve, reject) {
+            reject(e);
+        });
+    }
+
+    opts.from = opts.from || 'auto';
+    opts.to = opts.to || 'en';
+
+    opts.from = getCode(opts.from);
+    opts.to = getCode(opts.to);
+
+    return getGoogleToken(text).then(function (token) {
+        var url = 'https://translate.google.com/translate_a/single';
+        /*
+        var data = {
+            client: 't',
+            sl: opts.from,
+            tl: opts.to,
+            hl: opts.to,
+            dt: ['at', 'bd', 'ex', 'ld', 'md', 'qca', 'rw', 'rm', 'ss', 't'],
+            ie: 'UTF-8',
+            oe: 'UTF-8',
+            otf: 1,
+            ssel: 0,
+            tsel: 0,
+            kc: 7,
+            q: text
+        };
+        */
+        var data = {
+            client: 'gtx',
+            sl: opts.from,
+            tl: opts.to,
+            hl: opts.to,
+            dt: ['t', 'bd'],
+            dj: '1',
+            source: 'icon',
+            q: text
+        };
+        data[token.name] = token.value;
+
+        return url + '?' + stringify(data);
+    }).then(function (url) {
+        return httpGet(url, 'json', 'https://www.google.be/').then(function (res) {
+            var result = {
+                text: res.sentences[0].trans
+            };
+
+            return result;
+        }).catch(function (err) {
+            var e;
+            e = new Error();
+            if (err && err.statusCode !== undefined && err.statusCode !== 200) {
+                e.code = 'BAD_REQUEST';
+            } else {
+                e.code = 'BAD_NETWORK';
+            }
+            throw e;
+        });
+    });
+}
---
+++ vocabulary-api.js
@@ -0,0 +1,353 @@
+/** 
+ * Promise based API interface for Vocabulary.com
+ * 
+ * Uses Vocabulary.com's unofficial JSON API where possible.
+ * Falls back on HTML extraction for definitions and word list content.
+ * */
+class VocAPI {
+
+    constructor() {
+        this.PROTOCOL = 'https';
+        this.HOST = 'www.vocabulary.com';
+        this.URLBASE = `${this.PROTOCOL}://${this.HOST}`;
+        this.listNameCache = {};
+
+        this.loggedIn = false;
+    }
+    
+    /**
+     * log-in check
+     */
+    checkLogin() {
+        if (!this.loggedIn) {
+            const requestUrl = `${this.URLBASE}/account/progress`;
+            var req = new XMLHttpRequest();
+            req.open("GET", requestUrl, true);
+            //req.withCredentials = true;
+            req.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+            req.responseType = "json";
+            
+            return new Promise((resolve, reject) => {
+                req.onload = function () {
+                    if (req.responseURL !== requestUrl) { // response url was not same as requested url: 302 login redirect happened
+                        reject('not logged in');
+                    } else {
+                        loggedIn = true;
+                        resolve('already logged in');
+                    }
+                }
+                req.send();
+            })
+        } else {
+            return Promise.resolve('already logged in');
+        }
+    }
+
+    getListName(id) {
+        if (id in this.listNameCache) {
+            return Promise.resolve(this.listNameCache[id]);
+        } else {
+            return Promise.reject('Name not found in cache');
+        }
+    }
+
+    getListNameSync(id) {
+        if (id in this.listNameCache) {
+            return this.listNameCache[id];
+        } 
+    }
+
+    /**
+     * @returns definition in the format
+     * 
+     * {
+     *  "word": string,
+     *  "definition", string      // primary definition (as given by the meta description tag)
+     *  "description": string     // the long format description unique to Vocabulary.com
+     *  "audioURL": string        // of the form https://audio.vocab.com/1.0/us/C/12RWPXAD427B5.mp3
+     *                               // with C/.... being the code, in data-audio on the element
+     *                               // document.querySelector('.audio')
+     *  "meanings": [       // categories of meanings
+     *      {                   // specific meaning
+     *          "forms": [          // different forms
+     *              {
+     *                  "pos": string,          // part of speech: v, n, ...                
+     *                  "definition": string,
+     *                  "synonyms": [ synonym ],           // TODO: kunnen ook meerdere zijn?
+     *                  "subtypes": [                      // subtypes of the word
+     *                      { "words": [ string ],   // subtypes
+     *                        "defintition": string:
+     *                      }
+     *                  "supertypes":                   // TODO: only 1?
+     *                  {
+     *                       "words": [ string ]
+     *                       "definition": string
+     *                  }
+     *              }
+     *            ]
+     *         }     
+     *      ]
+     *    }
+     *  ]
+     * }
+     * 
+     * Primary meanings are, for every meaning, the first form of a given part of speech
+     */
+    getDefinition(word) {
+        /* stub */
+        return "Not implemented."
+    }
+
+    /**
+     * 
+     */
+    getLists() {
+        return new Promise( (resolve, reject) => {
+            const refererUrl = `${this.URLBASE}/dictionary/hacker`; 
+            const requestUrl = `${this.URLBASE}/lists/byprofile.json`;
+
+            // options: name, createdate, wordcount, activitydate TODO: make options
+            let sortBy = "modifieddate"
+
+            VocAPI.withModifiedReferrer(refererUrl, requestUrl, (detachHook) => {
+                var req = new XMLHttpRequest();
+                req.open("GET", requestUrl, true);
+                req.withCredentials = true;
+                req.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+                req.responseType = "json";
+                // TODO: this versus onload?
+                req.onreadystatechange = () => {
+                    if (req.readyState == 4 && req.status == 200) {
+                        detachHook();
+                        
+                        const lists = req.response.result.wordlists
+                            .filter(wl => wl.owner)
+                            .sort((a,b) => a[sortBy] > b[sortBy] ? -1 : 1); // high to low
+                        
+                        // fill cache with names
+                        lists.forEach(wl => {
+                            this.listNameCache[wl.wordlistid] = wl.name;
+                        })
+                        resolve(lists);
+                    }
+                    else if (req.status != 200) {
+                        console.log(`Error: ` + req.responseText);
+                        detachHook();
+                        reject();
+                    }
+                }
+                req.send();
+                }); 
+        }
+        );
+    }
+
+    /**
+     * @param wordToLearn as a plain word
+     */
+    startLearning(wordToLearn) {
+        return new Promise((resolve, reject) => {
+            console.log("Trying to learn " + wordToLearn)
+            const refererUrl = `${this.URLBASE}/dictionary/${wordToLearn}`; 
+            const requestUrl = `${this.URLBASE}/progress/startlearning.json`;
+        
+            VocAPI.withModifiedReferrer(refererUrl, requestUrl, (detachHook) => {
+              var req = new XMLHttpRequest();
+              req.open("POST", requestUrl, true);
+              req.withCredentials = true;
+              req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+              req.onreadystatechange = function () {
+                if (req.readyState == 4 && req.status == 200) {
+                    detachHook();
+                    resolve(req.responseText);
+                }
+                else if (req.status != 200) {
+                    detachHook();
+                    reject();
+                    console.log(`Error: ` + req.responseText);
+                }
+              }
+              req.send(`word=${wordToLearn}`);
+            });
+        });
+    }
+
+    /**
+     * Maps words from this interface's format to voc.com's format
+     * Adds some obvious info
+     * @param {} w 
+     */
+    static wordMapper(w) {
+        let nw = {
+        "word": w.word,
+        "lang": "en"
+        }
+        w.description ? nw["description"] = w.description : false;
+        w.example ? nw["example"] = { "text": w.example } : false;
+        return nw;
+    }
+
+    /** 
+    * @param words an array of words to add to the list - format:
+    * [
+        {
+            "word":"kangaroo",
+            "description":"Test kangaroo", 
+            "example": "Kangaroo makes me boo"
+        }
+        ]
+        description and example are optional
+    * @param listId id of the listlist
+    */ 
+    addToList(words, listId) {
+        return new Promise((resolve, reject) => {
+            console.log("Trying to save " + words)
+            const refererUrl = `${this.URLBASE}/dictionary/${words[0]}`; 
+            const requestUrl = `${this.URLBASE}/lists/save.json`;
+          
+            VocAPI.withModifiedReferrer(refererUrl, requestUrl, (detachHook) => {
+              var req = new XMLHttpRequest();
+              req.open("POST", requestUrl, true);
+              req.withCredentials = true;
+              req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+              let wordObjects = words.map(VocAPI.wordMapper);
+
+              req.onload = function () {
+                if (req.status == 200) {
+                    console.log(req.responseText);
+                    detachHook();
+                    resolve(req.responseText);
+                }
+                else if (req.status != 200) {
+                    detachHook();
+                    reject(req.responseText);        
+                    console.log(`Error: ` + req.responseText);
+                }
+               }
+              const toSend = {
+                  "addwords": JSON.stringify(wordObjects),
+                  "id": listId 
+              }
+              req.send(VocAPI.getFormData(toSend));
+            });
+        });
+    }
+
+    /** 
+    * @param words an array of words to add to the new list
+    * @param listName name of the new list
+    * @param description description of the list
+    * @param shared boolean that shows whether list should be shared or not
+    */ 
+    addToNewList(words, listName, description, shared) {
+        return new Promise((resolve, reject) => {
+            const refererUrl = `${this.URLBASE}/lists/vocabgrabber`; 
+            const requestUrl = `${this.URLBASE}/lists/save.json`;
+          
+            let listObj = {
+              //"words": words.map((w) => { return {"word": w} }),
+              "words": words.map(VocAPI.wordMapper),
+              "name": listName,
+              "description": description,
+              "action": "create",
+              "shared": shared
+            }
+          
+            VocAPI.withModifiedReferrer(refererUrl, requestUrl, (detachHook) => {
+              var req = new XMLHttpRequest();
+              req.open("POST", requestUrl, true);
+              req.responseType = "json";
+              req.withCredentials = true;
+              req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+          
+              req.onload = function () {
+                if (req.status == 200) {
+                    console.log(req.response);
+                    detachHook();
+                    resolve(req.response);                    
+                } else if (req.status != 200) {
+                    detachHook()
+                    console.log(`Error: ` + req.response);
+                    reject(req.response);
+                }
+               }
+              req.send(VocAPI.getFormData({'wordlist': JSON.stringify(listObj)}));
+            });
+        });
+    }
+
+    static translation(requestUrl, modifiedReferrer) {
+        return new Promise((resolve, reject) => {
+            let action = (detachHook) => {
+                var req = new XMLHttpRequest();
+                req.withCredentials = false;
+                req.open("GET", requestUrl, true);
+                req.responseType = 'json';
+                req.onload = function () {
+                    if (detachHook) detachHook();
+                    if (req.status == 200) {
+                        resolve(req.response);
+                    }
+                    else if (req.status != 200) {
+                        reject();
+                    }
+                 }
+                req.send();
+            };
+            VocAPI.withModifiedReferrer(modifiedReferrer, requestUrl, action);
+        });
+    }
+
+    /**
+     * Execute an function with a modified Referer header for browser requests
+     * @param {*} refererUrl the referer URL that will be injected
+     * @param {*} requestUrl the request URL's for which the header has to be injected
+     * @param {*} action the action (request) to be executed. 
+     *                  Gets passed a function that will detach the header modifier hook if called
+     */
+    static withModifiedReferrer(refererUrl, requestUrl, action) {
+        function refererListener(details) {
+            const i = details.requestHeaders.findIndex(e => e.name.toLowerCase() == "referer");
+            if (i != -1) {
+                details.requestHeaders[i].value = refererUrl;
+            } else {
+                details.requestHeaders.push({name: "Referer", value: refererUrl});
+            }
+            // Firefox uses promises
+            // return Promise.resolve(details);
+            // Chrome doesn't. Todo: https://github.com/mozilla/webextension-polyfill
+        
+            // important: do create a new object, passing the modified argument does not work
+            return {requestHeaders: details.requestHeaders};
+        }
+
+        // modify headers with webRequest hook
+        chrome.webRequest.onBeforeSendHeaders.addListener(
+        refererListener, //  function
+        {urls: [requestUrl]}, // RequestFilter object
+        ["requestHeaders", "blocking"] //  extraInfoSpec
+        );
+
+        // TODO:    why not hook detach after action? async?
+        //          hook should be detached after the request was sent, automatically   
+        action(() => {
+        // detach hook
+        if (chrome.webRequest.onBeforeSendHeaders.hasListener(refererListener)) {
+            chrome.webRequest.onBeforeSendHeaders.removeListener(refererListener)
+        }
+        });
+    }
+
+    /**
+     * Transforms objects of the form {"key": value, "key2": value2} to the form key=value&key2=value2
+     * With the values interpreted as strings. They are URL encoded in the process.
+     * @param {*} object 
+     */
+    static getFormData(object) {
+        // const formData = new FormData();
+        // Object.keys(object).forEach(key => formData.append(key, object[key]));
+        let returnString = '';
+        Object.keys(object).forEach((key, index) => returnString += `${index === 0 ? '' : '&'}${key}=${encodeURIComponent(object[key])}`)
+        return returnString;
+        }
+}
