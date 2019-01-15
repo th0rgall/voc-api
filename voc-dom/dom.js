7d685a0d1b3b74f2077b2c813121cb53f9fd8efc
Thor Galle <thor.galle@student.kuleuven.be>
2019-01-15 14:54:08 +0200

    Move utility repositories

--- voc-dom/dom.js
+++
@@ -1,8 +0,0 @@
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-
-module.exports = function parseDocument(htmlText) {
-    const dom = new JSDOM(htmlText);
-    return dom.window.document;
-}
-
